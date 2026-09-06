0dd458c4fb95f43ab0bd21bdeedac62c7578aa29
Esinstall2026 <edvanio.souza@gmail.com>
2026-09-05 20:52:43 -0400

    feat(access): route Admin and User profiles at app entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import EnhancedRoot from "./EnhancedRoot";
+import UserPortal from "./UserPortal";
+import { AccessGate, getSession } from "./Auth";
 import "./styles.css";
+
+function RoleRoot() {
+  const session = getSession();
+  return session?.role === "User" ? <UserPortal /> : <EnhancedRoot />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <EnhancedRoot />
+    <AccessGate>
+      <RoleRoot />
+    </AccessGate>
   </React.StrictMode>
 );
